6ef8255d71b1c9218ed7eeffcd045f8b73da96a0
Pete <landahoy55@gmail.com>
2018-02-28 09:54:23 +0000

    add question and answer text to labels

--- Controller/ProtoQuizViewController.swift
+++ Controller/ProtoQuizViewController.swift
@@ -16,16 +16,34 @@ class ProtoQuizViewController: UIViewController {
     var questionIndex = 0
     //current question - optional to start with.
     var currentQuestion: Question!
-    
-    
     //time, score
     var timer = Timer()
     var score = 0
     
+    //Outlets
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    //add to array?
+    var buttons = [UIButton]() //add buttons?
+    @IBOutlet weak var answerButton0: UIButton!
+    @IBOutlet weak var answerButton1: UIButton!
+    @IBOutlet weak var answerButton2: UIButton!
+    @IBOutlet weak var answerButton3: UIButton!
+    
+    @IBOutlet weak var timerProgressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //append buttons
+        buttons.append(answerButton0)
+        buttons.append(answerButton1)
+        buttons.append(answerButton2)
+        buttons.append(answerButton3)
+        
+        //Move in a bit.
+        loadQuestion()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -34,13 +52,27 @@ class ProtoQuizViewController: UIViewController {
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func loadQuestion() {
+        currentQuestion = subTopic?.questions[questionIndex]
+        setQuestionLayout()
+    }
+    
+    //including buttons
+    func setQuestionLayout() {
+        questionLabel.text = currentQuestion.question
+        for (index, button) in buttons.enumerated() {
+            print(button.tag)
+            print(currentQuestion.answers[index].answer)
+            button.setTitle(currentQuestion.answers[index].answer, for: .normal)
+        }
     }
     
     @IBAction func closeBtn(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func answerPressed(_ sender: UIButton) {
+        
     }
     
 }
