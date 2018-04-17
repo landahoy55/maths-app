eb9e58c829411506307795028025d21e87268852
Pete <landahoy55@gmail.com>
2018-04-17 14:44:37 +0100

    add medals

--- View/menuCollectionViewCell.swift
+++ View/menuCollectionViewCell.swift
@@ -13,6 +13,7 @@ class menuCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var medalImage: UIImageView!
     
     
     func configureCell(title: String, description: String, numberAchieved: Int){
@@ -30,25 +31,32 @@ class menuCollectionViewCell: UICollectionViewCell {
         switch numberAchieved {
         case 0:
             display = "0/5 complete 🙃"
+            medalImage.image = UIImage(named: "No.png")
             //scoreLabel.textColor = .white
             //scoreLabel.blinking()
         case 1:
             display = "1/5 complete 👎"
+            medalImage.image = UIImage(named: "No.png")
             //scoreLabel.textColor = timerRed
         case 2:
             display = "2/5 complete 🤔"
+            medalImage.image = UIImage(named: "No.png")
             //scoreLabel.textColor = timerOrange
         case 3:
             display = "3/5 complete ☺️"
+            medalImage.image = UIImage(named: "Bronze.png")
             //scoreLabel.textColor = timerOrange
         case 4:
             display = "4/5 complete 😘"
+            medalImage.image = UIImage(named: "Silver.png")
             //scoreLabel.textColor = timerGreen
         case 5:
             display = "5/5 complete 😍"
+            medalImage.image = UIImage(named: "Gold.png")
             //scoreLabel.textColor = timerGreen
         default:
             display = "Try now!"
+            medalImage.image = UIImage(named: "No.png")
             //scoreLabel.textColor = .white
             //scoreLabel.blinking()
         }
